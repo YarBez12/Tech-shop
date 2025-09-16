aefd4677ce31be0f66ec21c15a4f51512269dcbc
YarBez12 <yaroslav.bezvesilnyi@nure.ua>
2025-09-16 22:49:25 +0100

    Refactored PRODUCTS app

--- static/js/review.js
+++ static/js/review.js
@@ -1,13 +1,4 @@
 $u.onReady(()=>{
-  $u.delegate(document, '.delete-image', 'click', async (e, btn)=>{
-    e.preventDefault();
-    const imageId = btn.dataset.imageId;
-    const url = `/products/review-image/${imageId}/delete/`;
-    const res = await $u.csrfFetch(url, { body: JSON.stringify({ id: imageId }) });
-    if (res.ok) btn.closest('.image-container')?.remove();
-    else console.error('Deletion failed');
-  });
-
   const g = document.querySelector('#review-edit-gallery');
   if (g) lightGallery(g, { selector:'a', plugins:[lgZoom, lgThumbnail], speed:400 });
 });
